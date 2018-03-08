350e20f30c85c7431b0c1f068e8297e83c065b70
kkneville <mskatherinekrueger@gmail.com>
2018-03-08 12:41:34 -0600

    working on login component.ts

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  	private _UserService: UserService,
+  	private _route: Router
+  ) { }
 
   ngOnInit() {
   }
+
+
+  user = new User()
+
+  onSubmit(event){
+  	event.preventDefault();
+  	console.log('attempting to log in')
+  	this._UserService.addUser(this.user, (user) => {
+  		if (user) {
+        console.log(user);
+        this._route.navigateByUrl('/shop') 
+      }
+    });
+  }
+
 
 }

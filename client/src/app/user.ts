d00a1a1e865a8e8bb235d9db45bdfbd6702bc54f
kkneville <mskatherinekrueger@gmail.com>
2018-03-12 08:05:05 -0500

    user model in client

--- client/src/app/user.ts
+++ client/src/app/user.ts
@@ -1,6 +1,9 @@
 export class User {
 	constructor (
-		public name: string = "",
+		public firstname: string = "",
+		public lastname: string = "",
+		public email: string = "",
+		password: string = "",
 		public level: number = 1,
 		public created_at: Date = new Date(),
 		public updated_at: Date = new Date()

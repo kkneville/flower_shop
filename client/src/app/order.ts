cca5203dd2e79f7f9cc6fc5d73d70afc35e1272f
kkneville <mskatherinekrueger@gmail.com>
2018-03-14 10:14:07 -0500

    added order model to client side

---
+++ client/src/app/order.ts
@@ -0,0 +1,20 @@
+export class Order{
+	constructor (
+		public size: string = "",
+		public cost: number = 0,
+		public due_date: Date = new Date(),
+		public colors: [],
+		public message: string = "",
+		public add_ons: [],
+		public florist_name: string = "",
+		public shipping: boolean = True,
+		public recipient: string = "",
+		public address1: string = "",
+		public address2: string = "",
+		public city: string = "",
+		public state: string = "",
+		public zip: number = 0,
+		public created_at: Date = new Date(),
+		public updated_at: Date = new Date()
+	){}
+}
